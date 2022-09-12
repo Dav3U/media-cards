361fa7902bedd565f8678c31ff36c189e26df128
Dave3U <davidujile@icloud.com>
2022-09-12 08:29:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,93 @@
+// import React from 'react';
+/* jshint esversion: 6 */
+import {useEffect, useState} from 'react';
+import MediaCard from './MediaCard';
+import './App.css';
+import SearchIcon from './search.svg';
+
+
+//63a3e8dd
+
+
+const API_URL = 'http://www.omdbapi.com?apikey=63a3e8dd';
+
+// const movie1= {
+//         "Title": "Superman, Spiderman or Batman",
+//         "Year": "2011",
+//         "imdbID": "tt2084949",
+//         "Type": "movie",
+//         "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+// }
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState([]);
+
+    const searchMovies = async(title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+        
+        setMovies(data.Search);
+    }
+
+    useEffect(()=>{
+        // searchTerm = "Latest Movies"
+        searchMovies('Transformers');
+        // searchMovies(searchTerm);
+
+    },[]);
+
+    
+
+    return(
+        <div className="app">
+
+        <h1>Media Hub</h1>
+
+    <div className="search">
+        <input 
+        placeholder= "Click here to search media" value= {searchTerm}
+        onChange = {(e)=> setSearchTerm(e.target.value)
+            
+        }
+
+        
+        onKeyUp ={
+            ()=> searchMovies(searchTerm)
+        }
+       
+        />
+
+        <img 
+        src={SearchIcon}
+        alt="Search"
+        // onClick= {
+        // ()=> searchMovies(searchTerm)
+        // }
+    />
+    </div>
+    {
+        movies && movies.length > 0
+        ?(
+         <div className= "container">
+             {movies.map((movie)=> ( 
+                <MediaCard movie = {movie} />
+        ))}
+        </div>
+
+        ):(
+        <div className="empty">
+            <h2> There are no movies found, please re-enter search term </h2>
+        </div>
+        )}
+   
+    </div>
+
+        
+    );
+}
+
+export default App;
---
+++ src/MediaCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+
+const MediaCard = ({movie})=>{
+
+    const handleClick = () => {
+        window.open("www.imdb.com/title/"+movie.imdbID);
+      };
+    
+    return(
+        <div className="movie"
+        onClick={handleClick}
+        >
+        
+
+    <div>
+        <p>{movie.Year}</p>
+        {
+        // console.log("www.imdb.com/title/"+movie.imdbID)
+        }
+       
+
+    </div>
+
+    <div>
+    <img src={movie.Poster!=='N/A' ? movie.Poster :'https://via.placeholder.com/400'} alt={movie.Title}/>
+    </div>
+
+    <div>
+    <span> {movie.Type} </span>    
+    <h3> {movie.Title} </h3>
+    </div>
+
+    </div>
+
+    )
+}
+export default MediaCard;
